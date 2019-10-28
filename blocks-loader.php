c6437d792c688325f558efd6bd2b130db66c135b
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2019-10-28 12:45:48 +0000

    hello world

---
+++ blocks-loader.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Hooks the blocks component to WordPress.
+ *
+ * @package     posterno-blocks
+ * @copyright   Copyright (c) 2019, Sematico, LTD
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       0.1.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
