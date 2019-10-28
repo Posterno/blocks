3fd144f6546a5d0c4b8817eec5743dbc53f39f04
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2019-10-28 15:41:00 +0000

    prototype block registration

--- blocks-loader.php
+++ blocks-loader.php
@@ -10,3 +10,35 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
+
+function wordpress_blocks_starter_block_categories( $categories, $post ) {
+	return array_merge(
+		$categories,
+		array(
+			array(
+				'slug'  => 'posterno',
+				'title' => 'Posterno',
+			),
+		)
+	);
+}
+add_filter( 'block_categories', 'wordpress_blocks_starter_block_categories', 10, 2 );
+
+function wordpress_blocks_starter_enqueue_block_editor_assets() {
+
+	wp_enqueue_script(
+		'posterno-blocks',
+		PNO_PLUGIN_URL . 'vendor/posterno/blocks/build/index.js',
+		[
+			'wp-blocks',
+			'wp-i18n',
+			'wp-element',
+			'wp-components',
+			'wp-editor',
+		],
+		PNO_VERSION,
+		true
+	);
+
+}
+add_action( 'enqueue_block_editor_assets', 'wordpress_blocks_starter_enqueue_block_editor_assets' );
